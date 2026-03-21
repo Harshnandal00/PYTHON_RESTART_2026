89332ce75c889341a1900e94568b145b2c404927
Harsh Nandal <harshnandal000@gmail.com>
2026-03-21 14:46:30 +0530

    Done this topic

--- src/01_basics.py
+++ src/01_basics.py
@@ -1,2 +1,3 @@
+#this is my first python program
 print("Pycharm Setup Complete!")
-
+print("it's really good to use!")
---
+++ src/02_variables.py
@@ -0,0 +1,52 @@
+# Variable = A container for a value (string, integer, float, boolean)
+#            A variable behaves as if it was the value it contains
+
+#Strings datatypes..............
+first_name = "Harsh" #String data type.
+food = "Rice & curry"
+gmail = "harsh123@gmail.com"
+
+#first_name variable now behave as a string.
+#f-string - use to print our variable along with some texts
+
+# print(f"Hello {first_name}")
+# print(f"you like {food}!")
+# print(f"your gmail is {gmail}!")
+
+#Integers
+age = 25
+quantity = 3
+num_of_students = 40
+
+# print(f"you are {age} years old")
+# print(f"you are buying {quantity} packets of chips")
+# print(f"your class has {num_of_students} students")
+
+#floats
+price = 10.99
+gpa = 3.2
+distance = 49.2
+
+# print(f"The price is ${price}")
+# print(f"your gpa is {gpa}")
+# print(f"you ran {distance}km")
+
+#boolean  (True or False) / (1 or 0)
+is_student = False
+for_sale = True
+is_online = True
+
+# if is_student:
+#     print("you are a student")
+# else:
+#     print("you are not a student")
+
+# if for_sale:
+#     print("That item is for sale")
+# else:
+#     print("That item is not for sale")
+
+# if is_online:
+#     print("You are online")
+# else:
+#     print("You are offline")
