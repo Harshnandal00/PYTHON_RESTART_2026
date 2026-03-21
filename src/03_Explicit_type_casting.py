902c662deafba572bdc127b38ae29cc651edba75
Harsh Nandal <harshnandal000@gmail.com>
2026-03-21 15:29:44 +0530

    Type_Casting Topic

---
+++ src/03_Explicit_type_casting.py
@@ -0,0 +1,46 @@
+#typecasting = The process of converting a value of one data type to another
+#              (string , integer , float , boolean)
+#               Explicit vs implicit
+
+
+name = "Harsh"
+age = 18
+gpa = 1.9
+student = True
+
+# print(type(name))
+# print(type(age))
+# print(type(gpa))
+# print(type(student))
+
+#Explicit typecasting
+
+# # Int -> Float
+# age = float(age)
+# print(type(age))
+# print(age)
+
+# # Float -> Int
+# # decimal part is lost during typecasting
+# gpa = int(gpa)
+# print(type(gpa))
+# print(gpa)
+
+# # Bool -> Str
+# # It may seem that output didn't change
+# # but now the Truth is treated as a string
+# student = str(student)
+# print(student)
+# print(type(student))
+
+# # Int -> Bool
+# # It will always give True as output if value is non-zero
+# age = bool(age)
+# print(type(age))
+# print(age)
+
+# # String -> Bool
+# # If Str is Empty , Bool = False , otherwise True
+# name = bool(name)
+# print(type(name))
+# print(name)
---
+++ src/04_Implicit_type_casting.py
@@ -0,0 +1,12 @@
+#typecasting = The process of converting a value of one data type to another
+#              (string , integer , float , boolean)
+#               Explicit vs implicit
+
+# Implicit typecasting:
+# when a variable's datatype changes internally automatically
+# when we perform a mathematical operations on it
+
+x = 2
+y = 2.0
+x = x / y
+print(x) #converted into float automatically internally.
