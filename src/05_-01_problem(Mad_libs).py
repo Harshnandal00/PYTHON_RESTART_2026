d007f13bdb3530dff91daeb5e3e7cfa3157d696d
Harsh Nandal <harshnandal000@gmail.com>
2026-03-25 14:51:57 +0530

    Problem on UserInput

---
+++ src/05_-01_problem(Mad_libs).py
@@ -0,0 +1,11 @@
+adjective = input("Enter the adjective: ")
+noun = input("Enter the noun: ")
+adjective2 = input("Enter the adjective: ")
+verb = input("Enter the verb: ")
+adjective3 = input("Enter the adjective: ")
+
+
+print(f"Today I went to a {adjective} zoo. ")
+print(f"in an exhibit, I saw {noun}")
+print(f"{noun} was {adjective2} and {verb}ing")
+print(f"i was {adjective3}")
