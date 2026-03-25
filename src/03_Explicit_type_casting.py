6f17a5d7697e07046e0df8724c23622dd8576bd0
Harsh Nandal <harshnandal000@gmail.com>
2026-03-25 14:10:44 +0530

    Edited_Explicit_type_casting

--- src/03_Explicit_type_casting.py
+++ src/03_Explicit_type_casting.py
@@ -8,39 +8,35 @@ age = 18
 gpa = 1.9
 student = True
 
-# print(type(name))
-# print(type(age))
-# print(type(gpa))
-# print(type(student))
 
-#Explicit typecasting
+# Explicit typecasting
 
-# # Int -> Float
-# age = float(age)
-# print(type(age))
-# print(age)
+# Int -> Float
+age = float(age)
+print(type(age))
+print(age)
 
-# # Float -> Int
-# # decimal part is lost during typecasting
-# gpa = int(gpa)
-# print(type(gpa))
-# print(gpa)
+# Float -> Int
+# decimal part is lost during typecasting
+gpa = int(gpa)
+print(type(gpa))
+print(gpa)
 
-# # Bool -> Str
-# # It may seem that output didn't change
-# # but now the Truth is treated as a string
-# student = str(student)
-# print(student)
-# print(type(student))
+# Bool -> Str
+# It may seem that output didn't change
+# but now the Truth is treated as a string
+student = str(student)
+print(student)
+print(type(student))
 
-# # Int -> Bool
-# # It will always give True as output if value is non-zero
-# age = bool(age)
-# print(type(age))
-# print(age)
+# Int -> Bool
+# It will always give True as output if value is non-zero
+age = bool(age)
+print(type(age))
+print(age)
 
-# # String -> Bool
-# # If Str is Empty , Bool = False , otherwise True
-# name = bool(name)
-# print(type(name))
-# print(name)
+# String -> Bool
+# If Str is Empty , Bool = False , otherwise True
+name = bool(name)
+print(type(name))
+print(name)
