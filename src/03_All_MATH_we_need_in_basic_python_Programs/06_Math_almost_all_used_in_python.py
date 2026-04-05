0e94fd00d5ee98d5d908ed8c740492a6e55ce61a
Harsh Nandal <harshnandal000@gmail.com>
2026-04-05 17:08:59 +0530

    Managed_folders_

---
+++ src/03_All_MATH_we_need_in_basic_python_Programs/06_Math_almost_all_used_in_python.py
@@ -0,0 +1,24 @@
+
+#....ARITHMATIC OPERATORS....#
+
+friends = 5
+
+# friends = friends + 1
+# friends += 1
+
+# friends = friends - 1
+# friends -= 1
+
+# friends = friends * 3
+# friends *= 3
+
+# friends = friends / 2
+# friends /= 2
+
+# friends = friends ** 2
+# power of 2 on friends
+# friends **= 2
+
+remainder = friends % 3
+
+print(remainder)
